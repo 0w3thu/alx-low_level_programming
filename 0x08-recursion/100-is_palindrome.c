48ed42c51a29d797ca8d810429270328e9a37fec
0w3thu <owethukhumalo403@gmail.com>
2023-10-01 09:40:16 -0700

    C Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * palind2 - obtains length of a
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * prime2 - Makes possible to evaluate from 1 to n
@@ -8,24 +8,25 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int prime2(int a, int b)
-{
-	if (a == b)
-		return (1);
-	else if (a % b == 0)
-		return (0);
-	return (prime2(a, b + 1));
-}
-/**
- * is_prime_number - checks if a number is prime
- * @n: Number Integer
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (prime2(n, 2));
+	return (actual_prime(n, n - 1));
+}
+
+/**
+ * actual_prime - calculates if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i - 1));
 }
